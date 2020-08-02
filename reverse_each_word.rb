5205637248d2f199aacafe774996b6e2ebac01bf
Nkenge Simms <simmsn@yahoo.com>
2020-08-02 00:01:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
   array = string.split(" ")
-  string.collect do |word|
+  new_array = string.collect do |word|
      string << "Hello, there and how are you?"
   
 end
